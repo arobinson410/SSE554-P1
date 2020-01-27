87f66d43167b6c29c387fd5c7194130e1be5892d
Andrew <arobinson410@gmail.com>
2020-01-26 23:54:33 -0500

    Implemented pokemon import test

--- P1-UnitTest/UnitTest1.cs
+++ P1-UnitTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace P1_UnitTest
@@ -10,7 +11,16 @@ namespace P1_UnitTest
         public void checkPokemonCreation()
         {
             P1.Pokemon pokemon = new P1.Pokemon("Rhydon", P1.Pokemon.Type.Ground, P1.Pokemon.Type.Rock);
-            Assert.IsTrue(pokemon.Name.Equals("Rhydon") && pokemon.PrimaryType.Equals(P1.Pokemon.Type.Ground) && pokemon.SecondaryType.Equals(P1.Pokemon.Type.Rock));
+            Assert.IsTrue(pokemon.Name.Equals("Rhydon") && pokemon.PrimaryType.Equals(P1.Pokemon.Type.Ground) && pokemon.SecondaryType.Equals(P1.Pokemon.Type.Rock), "Test Pokemon Creation Successful");
+        }
+
+        [TestMethod]
+        public void checkImport()
+        {
+            List<P1.Pokemon> pokemonList = new List<P1.Pokemon>();
+            pokemonList.AddRange(P1.FileReader.getPokemonFromFile(@"D:\Users\arobi\Desktop\pokemondatabase.txt"));
+            Assert.AreEqual(pokemonList.Count, 151, "Pokemon list not of expected size");
+
         }
     }
 }
--- P1/FileReader.cs
+++ P1/FileReader.cs
@@ -10,8 +10,7 @@ namespace P1
     {
         public static Pokemon[] getPokemonFromFile(string filename)
         {
-            string[] lines = System.IO.File.ReadAllLines(filename);
-            return new Pokemon[1];
+            throw new NotImplementedException();
         }
 
     }
